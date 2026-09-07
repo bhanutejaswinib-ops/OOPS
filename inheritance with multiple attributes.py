d027912cb94f44ef0870fc492e401a1e5524af83
bhanutejaswinib-ops <bhanutejaswinib@gmail.com>
2026-09-07 15:59:11 +0530

    inheritance with multiple attributes

---
+++ inheritance with multiple attributes.py
@@ -0,0 +1,9 @@
+class college:
+    def study(self):
+        print("College is place to study")
+class KPRIT(college):
+    def ghanpur(self):
+        print("KPRIT is in ghanpur")
+d1=KPRIT()
+d1.study()
+d1.ghanpur()
